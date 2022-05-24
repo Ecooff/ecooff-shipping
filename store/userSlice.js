10b68a50c50cdfcc57de7873ff45ec18485ec446
Tobías Matarasso <42041453+tobiasmatarasso@users.noreply.github.com>
2022-05-24 09:30:41 -0500

    App runing

--- store/userSlice.js
+++ store/userSlice.js
@@ -6,25 +6,15 @@ export const userSlice = createSlice({
     user: null,
   },
   reducers: {
-    register: (state, action) => {
-      state.user = action.payload;
-    },
     logout: (state, action) => {
       state.user = null;
     },
     login: (state, action) => {
       state.user = action.payload;
     },
-    updateName: (state, action) => {
-      state.user = {
-        ...state.user,
-        firstName: action.payload.firstName,
-        lastName: action.payload.lastName,
-      };
-    },
   },
 });
 
-export const { login, logout, register, updateName } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 export default userSlice.reducer;
